794dd2dd356737fa787c48ba1108379bdc318511
Andrew <andrewmreiblein013@gmail.com>
2021-03-16 09:47:22 +0200

    project update

---
+++ blogs/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Post
+
+
+# Create your views here.
+
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blogs/home.html', context)
+
+def about(request):
+    return render(request, 'blogs/about.html')
