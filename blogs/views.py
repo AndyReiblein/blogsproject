bc25b738948a0329b124336b6cb88626fd966f70
Andrew <andrewmreiblein013@gmail.com>
2021-03-25 15:54:17 +0200

    gitignore

--- blogs/views.py
+++ blogs/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
-
-
-# Create your views here.
-
 
 
 def home(request):
@@ -14,14 +16,17 @@ def home(request):
     }
     return render(request, 'blogs/home.html', context)
 
+
 class PostListView(ListView):
     model = Post
-    template_name = 'blogs/home.html'
+    template_name = 'blogs/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+
 class PostDetailView(DetailView):
     model = Post
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
@@ -30,6 +35,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
@@ -45,18 +51,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
 
     def test_func(self):
-       post = self.get_object()
-       if self.request.user == post.author:
-           return True
-       return False
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
-
-  
 
 def about(request):
-    return render(request, 'blogs/about.html')
+    return render(request, 'blogs/about.html', {'title': 'About'})
